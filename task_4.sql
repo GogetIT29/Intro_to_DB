7525550db161aacab84be0bf38d5dafbf8330a30
Esau Panky Mosia <pankymosia29@gmail.com>
2025-08-18 19:21:55 +0200

    Adds script for Task 4 to show table description

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Print the full description of the books table without using DESCRIBE or EXPLAIN
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
